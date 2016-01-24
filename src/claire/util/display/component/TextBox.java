55c33ab20e6547b59522cfb98d5957adfbeae6ac
Claire <foxcoindev@gmail.com>
2016-01-25 11:34:19 +1300

    Added TextBox

---
+++ src/claire/util/display/component/TextBox.java
@@ -0,0 +1,51 @@
+package claire.util.display.component;
+
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class TextBox 
+	   extends JTextField
+	   implements DocumentListener {
+	
+	private static final long serialVersionUID = 626015435330005349L;
+	
+	private boolean changed = false;
+
+	public TextBox() 
+	{
+		this.getDocument().addDocumentListener(this);
+	}
+
+	public TextBox(String arg0) 
+	{
+		super(arg0);
+		this.getDocument().addDocumentListener(this);
+	}
+
+	public void changedUpdate(DocumentEvent arg0)
+	{
+		changed = true;
+	}
+	
+	public void insertUpdate(DocumentEvent arg0)
+	{
+		changed = true;
+	}
+
+	public void removeUpdate(DocumentEvent arg0)
+	{
+		changed = true;
+	}
+	
+	public boolean hasChanged()
+	{
+		return this.changed;
+	}
+	
+	public void reset()
+	{
+		this.changed = false;
+	}
+
+}
