8d7a9f6736f6df02e5272befafc0a7e2f2b2762a
Claire <foxcoindev@gmail.com>
2016-01-30 23:52:54 +1300

    Removed key operation methods

--- src/claire/util/standards/crypto/IPublicKey.java
+++ src/claire/util/standards/crypto/IPublicKey.java
@@ -1,14 +1,7 @@
 package claire.util.standards.crypto;
 
-import claire.util.crypto.cipher.StateSpaceRequiredException;
-
 public interface IPublicKey<Key extends IPublicKey<Key>>
 	   extends IKey<Key> {
-
-	void encryptBlock(byte[] block, int start) throws StateSpaceRequiredException;
-	void encryptBlock(byte[] block, int start0, byte[] out, int start1) throws StateSpaceRequiredException;
-	
-	boolean verify(byte[] signature, int start0, byte[] message, int start1) throws StateSpaceRequiredException;
 
 	int messageSize();
 	int signatureSize();
