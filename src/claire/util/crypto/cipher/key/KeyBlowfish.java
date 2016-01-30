9d914a99aaae157db2f09836a6ff967cf3c64975
Claire <foxcoindev@gmail.com>
2016-01-31 02:54:36 +1300

    Reformed Blowfish [ERRORS]

--- src/claire/util/crypto/cipher/key/KeyBlowfish.java
+++ src/claire/util/crypto/cipher/key/KeyBlowfish.java
@@ -1,37 +1,92 @@
 package claire.util.crypto.cipher.key;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import claire.util.io.Factory;
+import claire.util.io.IOUtils;
+import claire.util.memory.util.ArrayUtil;
 import claire.util.standards._NAMESPACE;
+import claire.util.standards.crypto.IKey;
+import claire.util.standards.io.IIncomingStream;
+import claire.util.standards.io.IOutgoingStream;
 
-public class KeyBlowfish extends ByteKey<KeyBlowfish> {
-
-	public KeyBlowfish(byte[] key, int size) 
+public class KeyBlowfish 
+	   implements IKey<KeyBlowfish> {
+	
+	private byte[] bytes;
+	
+	public KeyBlowfish(byte[] bytes)
 	{
-		super(KeyBlowfish.class, key, size);
+		this.bytes = bytes;
 	}
 	
-	public KeyBlowfish(byte[] key)
-	{
-		super(KeyBlowfish.class, key);
-	}
-	
-	protected int getLength(byte[] bytes, int size)
-	{
-		if(size < 4)
-			return 4;
-		else if(size > 72)
-			return 72;
-		else
-			return size;
-	}
-
 	public int NAMESPACE()
 	{
 		return _NAMESPACE.KEYBLOWFISH;
 	}
 
-	protected KeyBlowfish construct(byte[] bytes)
+	public byte[] getBytes()
 	{
-		return new KeyBlowfish(bytes);
+		return this.bytes;
+	}
+	
+	public boolean sameAs(KeyBlowfish obj)
+	{
+		return ArrayUtil.equals(bytes, obj.bytes);
 	}
 
+	public KeyBlowfish createDeepClone()
+	{
+		return new KeyBlowfish(ArrayUtil.copy(bytes));
+	}
+
+	public void erase()
+	{
+		Arrays.fill(bytes, (byte) 0);
+		bytes = null;
+	}
+	
+	public void export(IOutgoingStream stream) throws IOException
+	{
+		stream.writeByteArr(bytes);
+	}
+
+	public void export(byte[] bytes, int offset)
+	{
+		IOUtils.writeArr(this.bytes, bytes, offset);
+	}
+
+	public int exportSize()
+	{
+		return bytes.length + 4;
+	}
+
+	public Factory<KeyBlowfish> factory()
+	{
+		return factory;
+	}
+
+	private static final KeyBlowfishFactory factory = new KeyBlowfishFactory();
+
+	private static final class KeyBlowfishFactory extends Factory<KeyBlowfish> {
+
+		protected KeyBlowfishFactory()
+		{
+			super(KeyBlowfish.class);
+		}
+
+		public KeyBlowfish resurrect(byte[] data, int start) throws InstantiationException
+		{
+			return new KeyBlowfish(IOUtils.readByteArr(data, start));
+		}
+
+		public KeyBlowfish resurrect(IIncomingStream stream) throws InstantiationException, IOException
+		{
+			return new KeyBlowfish(stream.readByteArr());
+		}
+		
+		
+		
+	}
 }
