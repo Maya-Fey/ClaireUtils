1e4fbbffea6dc31203e46774b94a927440857c50
Claire <foxcoindev@gmail.com>
2016-02-02 01:39:44 +1300

    Added default test methods for CObject

--- src/claire/util/standards/IDeepClonable.java
+++ src/claire/util/standards/IDeepClonable.java
@@ -1,7 +1,32 @@
 package claire.util.standards;
 
+import claire.util.logging.Log;
+
 public interface IDeepClonable<T> {
 	
 	public T createDeepClone();
+	
+	public static <T extends IDeepClonable<T> & IUUID<T>> int test(T obj)
+	{
+		T clo;
+		try {
+			clo = obj.createDeepClone();
+		} catch(Exception e) {
+			Log.err.println("Error: Exception encountered while deep cloning " + obj.getClass().getSimpleName());
+			return 1;
+		}
+		try {
+			if(clo.equals(obj)) {
+				return 0;
+			} else {
+				Log.err.println("Error: Cloned instances of " + obj.getClass().getSimpleName() + " were not equal in value.");
+				return 1;
+			}
+		} catch(Exception e) {
+			Log.err.println("Error: Exception encountered while comparing instances of " + obj.getClass().getSimpleName());
+			return 1;
+		}
+		
+	}
 
 }
--- src/claire/util/standards/IPersistable.java
+++ src/claire/util/standards/IPersistable.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 
 import claire.util.io.Factory;
 import claire.util.io.FileOutgoingStream;
+import claire.util.logging.Log;
+import claire.util.memory.buffer.ByteArrayIncomingStream;
+import claire.util.memory.buffer.ByteArrayOutgoingStream;
 import claire.util.standards.io.IOutgoingStream;
 
 public interface IPersistable<T> {
@@ -31,6 +34,46 @@ public interface IPersistable<T> {
 		FileOutgoingStream stream = new FileOutgoingStream(file);
 		stream.persist(this);
 		stream.close();
+	}
+	
+	public static <T extends IPersistable<T> & IUUID<T>> int test(T t)
+	{
+		try {
+			int e = 0;
+			Factory<T> f = t.factory();
+			byte[] bytes = new byte[t.exportSize()];
+			t.export(bytes, 0);
+			T t2 = f.resurrect(bytes, 0);
+			if(!t.equals(t2)) {
+				Log.err.println("Persisting and resurrecting from raw bytes did not yield the same object for class " + t.getClass().getSimpleName());
+				e++;
+			}
+			boolean b = false;
+			try {
+				t.export(bytes, 1);
+			} catch (Exception ex) {
+				b = true;
+			}
+			if(b) {
+				Log.err.println("Succeeded in persisting " + t.getClass().getSimpleName() + " with insufficient space, likely misreported required size");
+				e++;
+			}
+			ByteArrayOutgoingStream os = new ByteArrayOutgoingStream(bytes);
+			t.export(os);
+			T t3 = f.resurrect(new ByteArrayIncomingStream(bytes));
+			if(!t.equals(t3)) {
+				Log.err.println("Persisting and resurrecting from stream did not yield the same object for class " + t.getClass().getSimpleName());
+				e++;
+			}
+			if(!t2.equals(t3)) {
+				Log.err.println("Persisting and resurrecting from stream and raw bytes did not yield the same object for class " + t.getClass().getSimpleName());
+				e++;
+			}
+			return e;
+		} catch (Exception e) {
+			Log.err.println("An unexpected " + e.getClass().getSimpleName() + ": " + e.getMessage() + " occured while testing the persistability of " + t.getClass().getSimpleName());
+			return 1;
+		}
 	}
 	
 }
