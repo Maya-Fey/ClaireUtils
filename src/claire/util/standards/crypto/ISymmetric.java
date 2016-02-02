78ea0a43b008ee30771a34164155352337607d22
Claire <foxcoindev@gmail.com>
2016-02-03 08:54:43 +1300

    Updated symmetric test

--- src/claire/util/standards/crypto/ISymmetric.java
+++ src/claire/util/standards/crypto/ISymmetric.java
@@ -4,28 +4,58 @@ import claire.util.crypto.rng.RandUtils;
 import claire.util.logging.Log;
 import claire.util.memory.util.ArrayUtil;
 
-public interface ISymmetric<Key extends IKey<Key>>
+public interface ISymmetric<Key extends IKey<?>>
 	   extends ICrypto<Key>, 
 	   		   IEncrypter, 
 	   		   IDecrypter {
 	
-	public static int testSymmetric(ISymmetric<?> symm)
+	public static int testSymmetric(ISymmetric<? extends IKey<?>> cip, IKey<?> rep)
 	{
+		@SuppressWarnings("unchecked")
+		ISymmetric<IKey<?>> symm = (ISymmetric<IKey<?>>) cip;
+		int e = 0;
 		try {
 			byte[] b0 = new byte[symm.ciphertextSize()];
 			RandUtils.fillArr(b0);
 			byte[] b1 = ArrayUtil.copy(b0);
 			symm.encryptBlock(b1);
 			symm.decryptBlock(b1);
-			if(ArrayUtil.equals(b0, b1)) 
-				return 0;
-			else {
-				Log.err.println("Encryption followed by decryption does not yeild the original value for class " + symm.getClass().getSimpleName());
-				return 1;
+			if(!ArrayUtil.equals(b0, b1)) {
+				Log.err.println("Encryption followed by decryption does not yield the original value for class " + symm.getClass().getSimpleName());
+				e++;
 			}
-		} catch (Exception e) {
-			Log.err.println("An unexpected " + e.getClass().getSimpleName() + ": " + e.getMessage() + " occured while testing the functionality of " + symm.getClass().getSimpleName());
-			return 1;
+			symm.encryptBlock(b1);
+			symm.setKey(rep);
+			byte[] b2 = ArrayUtil.concat(b0);
+			symm.encryptBlock(b2);
+			if(ArrayUtil.equals(b1, b2)) {
+				Log.err.println("Changing the key had no effect for class " + symm.getClass().getSimpleName());
+				e++;
+			}
+			symm.decryptBlock(b2);
+			if(!ArrayUtil.equals(b0, b1)) {
+				Log.err.println("Encryption followed by decryption after key change does not yield the original value for class " + symm.getClass().getSimpleName());
+				e++;
+			}
+			
+			boolean suc = true;
+			try {
+				System.arraycopy(b0, 0, b1, 0, b0.length);
+				System.arraycopy(b0, 0, b2, 0, b0.length);
+				symm.encryptBlock(b1);
+				symm.wipe();
+				symm.encryptBlock(b2);
+			} catch(Exception ex) {
+				suc = false;
+			}
+			if(suc && ArrayUtil.equals(b1, b2)) {
+				Log.err.println("Encryption still works after wipe for " + symm.getClass().getSimpleName());
+				e++;
+			}
+			return e;
+		} catch (Exception ex) {
+			Log.err.println("An unexpected " + ex.getClass().getSimpleName() + ": " + ex.getMessage() + " occured while testing the functionality of " + symm.getClass().getSimpleName());
+			return e + 11;
 		}
 	}
 	
