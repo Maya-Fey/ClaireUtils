62389ea69dcd7a8e71aafb856fc6e80ad7ec84f7
Claire <foxcoindev@gmail.com>
2016-03-06 17:35:08 +1300

    Took full advantage of shorts in FEAL CI

--- src/claire/util/crypto/cipher/primitive/FEAL_CI.java
+++ src/claire/util/crypto/cipher/primitive/FEAL_CI.java
@@ -48,13 +48,13 @@ public class FEAL_CI
 		A[1] += A[0];
 		A[2] += A[3];
 		t = (short) (B[0] ^ A[2]);
-		A[1] = S1(A[1], t, (short) 0xAB04, 3);
+		A[1] = S1(A[1], t, (short) 0xAB04, 13);
 		t = (short) (A[1] ^ B[1]);
-		A[2] = S2(A[2], t, (short) 0x42D7, 7);
+		A[2] = S2(A[2], t, (short) 0x42D7,  9);
 		t = (short) (A[1] ^ B[2]);
-		A[0] = S2(A[0], t, (short) 0xFFEA, 5);
+		A[0] = S2(A[0], t, (short) 0xFFEA,  4);
 		t = (short) (A[2] ^ B[3]);
-		A[3] = S1(A[3], t, (short) 0x096F, 4);
+		A[3] = S1(A[3], t, (short) 0x096F,  7);
 	}
 	
 	private static void FR(short[] A, short[] B, short b1, short b2)
@@ -63,10 +63,10 @@ public class FEAL_CI
 		B[2] ^= b2;
 		B[1] += B[0];
 		B[2] += B[3];
-		B[1] = S1(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 7);
-		B[2] = S2(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 7);
-		B[0] = S2(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 7);
-		B[3] = S1(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 7);
+		B[1] = S1(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 15);
+		B[2] = S2(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 15);
+		B[0] = S2(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 15);
+		B[3] = S1(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 15);
 		short t = A[0];
 		A[0] = (short) (A[1] ^ B[0]);
 		A[1] = (short) (A[2] ^ B[1]);
@@ -80,10 +80,10 @@ public class FEAL_CI
 		B[2] ^= b2;
 		B[1] += B[0];
 		B[2] += B[3];
-		B[1] = S1(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 7);
-		B[2] = S2(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 7);
-		B[0] = S2(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 7);
-		B[3] = S1(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 7);
+		B[1] = S1(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 15);
+		B[2] = S2(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 15);
+		B[0] = S2(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 15);
+		B[3] = S1(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 15);
 		short t = A[3];
 		A[3] = (short) (A[2] ^ B[2]);
 		A[2] = (short) (A[1] ^ B[1]);
