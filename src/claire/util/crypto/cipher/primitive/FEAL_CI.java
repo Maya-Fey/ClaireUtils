63b4adc98e217091da76bb8f2a835e16b362a961
Claire <foxcoindev@gmail.com>
2016-03-06 13:34:18 +1300

    Improved FEAL_CI

--- src/claire/util/crypto/cipher/primitive/FEAL_CI.java
+++ src/claire/util/crypto/cipher/primitive/FEAL_CI.java
@@ -58,14 +58,15 @@ public class FEAL_CI
 		B[2] ^= b2;
 		B[1] += B[0];
 		B[2] += B[3];
-		B[1] = S(B[1], B[2], (short) 1, b1 & 7);
-		B[2] = S(B[2], B[1], (short) 0, b1 & 7);
-		B[0] = S(B[0], B[1], (short) 0, b2 & 7);
-		B[3] = S(B[3], B[2], (short) 1, b2 & 7);
-		A[0] ^= B[0];
-		A[1] ^= B[1];
-		A[2] ^= B[2];
-		A[3] ^= B[3];
+		B[1] = S(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 7);
+		B[2] = S(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 7);
+		B[0] = S(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 7);
+		B[3] = S(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 7);
+		short t = A[0];
+		A[0] = (short) (A[1] ^ B[0]);
+		A[1] = (short) (A[2] ^ B[1]);
+		A[2] = (short) (A[3] ^ B[2]);
+		A[3] = (short) (t    ^ B[3]);
 	}
 	
 	private static void FRI(short[] A, short[] B, short b2, short b1)
@@ -74,14 +75,15 @@ public class FEAL_CI
 		B[2] ^= b2;
 		B[1] += B[0];
 		B[2] += B[3];
-		B[1] = S(B[1], B[2], (short) 1, b1 & 7);
-		B[2] = S(B[2], B[1], (short) 0, b1 & 7);
-		B[0] = S(B[0], B[1], (short) 0, b2 & 7);
-		B[3] = S(B[3], B[2], (short) 1, b2 & 7);
-		A[0] ^= B[0];
-		A[1] ^= B[1];
-		A[2] ^= B[2];
-		A[3] ^= B[3];
+		B[1] = S(B[1], B[2], (short) (0xAB04 ^ B[0]), b1 & 7);
+		B[2] = S(B[2], B[1], (short) (0x42D7 ^ b1), B[3] & 7);
+		B[0] = S(B[0], B[1], (short) (0xFFEA ^ b2), B[0] & 7);
+		B[3] = S(B[3], B[2], (short) (0x096F ^ B[3]), b2 & 7);
+		short t = A[3];
+		A[3] = (short) (A[2] ^ B[2]);
+		A[2] = (short) (A[1] ^ B[1]);
+		A[1] = (short) (A[0] ^ B[0]);
+		A[0] = (short) (t    ^ B[3]);
 	}
 	
 	public void setKey(KeyFEAL_CI t)
@@ -189,10 +191,10 @@ public class FEAL_CI
 			FRI(A, C, schedule[--i], schedule[--i]);
 			r -= 2;
 		}
-		B[0] ^= A[0];
-		B[1] ^= A[1];
-		B[2] ^= A[2];
-		B[3] ^= A[3];
+		B[0] -= A[0];
+		B[1] -= A[1];
+		B[2] -= A[2];
+		B[3] -= A[3];
 		B[3] ^= schedule[--j];
 		B[2] ^= schedule[--j];
 		B[1] ^= schedule[--j];
@@ -243,10 +245,10 @@ public class FEAL_CI
 			FRI(A, C, schedule[--i], schedule[--i]);
 			r -= 2;
 		}
-		B[0] ^= A[0];
-		B[1] ^= A[1];
-		B[2] ^= A[2];
-		B[3] ^= A[3];
+		B[0] -= A[0];
+		B[1] -= A[1];
+		B[2] -= A[2];
+		B[3] -= A[3];
 		B[3] ^= schedule[--j];
 		B[2] ^= schedule[--j];
 		B[1] ^= schedule[--j];
@@ -274,10 +276,10 @@ public class FEAL_CI
 		B[1] ^= schedule[j++];
 		B[2] ^= schedule[j++];
 		B[3] ^= schedule[j++];
-		B[0] ^= A[0];
-		B[1] ^= A[1];
-		B[2] ^= A[2];
-		B[3] ^= A[3];
+		B[0] += A[0];
+		B[1] += A[1];
+		B[2] += A[2];
+		B[3] += A[3];
 		while(r > 1) {
 			System.arraycopy(B, 0, C, 0, 4);
 			FR(A, C, schedule[i++], schedule[i++]);
@@ -327,10 +329,10 @@ public class FEAL_CI
 		B[1] ^= schedule[j++];
 		B[2] ^= schedule[j++];
 		B[3] ^= schedule[j++];
-		B[0] ^= A[0];
-		B[1] ^= A[1];
-		B[2] ^= A[2];
-		B[3] ^= A[3];
+		B[0] += A[0];
+		B[1] += A[1];
+		B[2] += A[2];
+		B[3] += A[3];
 		while(r > 1) {
 			System.arraycopy(B, 0, C, 0, 4);
 			FR(A, C, schedule[i++], schedule[i++]);
