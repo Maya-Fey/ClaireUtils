9dde29281dd653a9b144c84a06b7db24254fbdb2
Claire <foxcoindev@gmail.com>
2016-03-26 18:00:03 +1300

    Added regression test for stream ciphers

--- src/claire/util/standards/crypto/IStreamCipher.java
+++ src/claire/util/standards/crypto/IStreamCipher.java
@@ -1,5 +1,8 @@
 package claire.util.standards.crypto;
 
+import claire.util.logging.Log;
+import claire.util.memory.util.ArrayUtil;
+
 public interface IStreamCipher<Type extends IKey<?>>
 	   extends ICrypto<Type> {
 	
@@ -28,4 +31,42 @@ public interface IStreamCipher<Type extends IKey<?>>
 			bytes[start++] ^= nextByte();
 		}
 	}
+	
+	public static int testCipher(IStreamCipher<?> cip)
+	{
+		int e = 0;
+		try {
+			byte b1 = cip.nextByte();
+			cip.reset();
+			if(b1 != cip.nextByte()) {
+				Log.err.println("Cipher resetting didn't work.");
+				e++;
+			}
+			cip.reset();
+			byte[] t1 = new byte[10];
+			byte[] t2 = new byte[10];
+			for(int i = 0; i < 10; i++)
+				t1[i] = cip.nextByte();
+			cip.reset();
+			cip.fill(t2, 0, 10);
+			if(!ArrayUtil.equals(t1, t2)) {
+				Log.err.println("Filling array manuall and using function yielded different results.");
+				e++;
+			}
+			cip.reset();
+			cip.wipe();
+			try {
+				if(b1 == cip.nextByte()) {
+					Log.err.println("Wiping the cipher had no effect");
+					e++;
+				}
+			} catch(Exception ex) {}
+		} catch (Exception ex) {
+			e++;
+			Log.err.println("Unexpected " + ex.getClass().getSimpleName() + ": " + ex.getMessage() + " encountered while testing " + cip.getClass().getSimpleName());
+			ex.printStackTrace();
+		}
+		return e;
+	}
+	
 }
