797fc4cf87d79bfea54859d851ab7e97408fc25d
Claire <foxcoindev@gmail.com>
2016-03-28 12:17:25 +1300

    Added MerkleStateFactory

--- src/claire/util/crypto/hash/primitive/MerkleHash.java
+++ src/claire/util/crypto/hash/primitive/MerkleHash.java
@@ -72,12 +72,16 @@ public abstract class MerkleHash<State extends MerkleState<State, Hash>, Hash ex
 		this.loadCustom(state);
 	}
 	
-	
-	
 	protected static abstract class MerkleState<State extends MerkleState<State, Hash>, Hash extends MerkleHash<State, Hash>> implements IState<State>
 	{
 		protected byte[] temp;
 		protected int pos;
+		
+		public MerkleState(byte[] bytes, int pos)
+		{
+			this.temp = bytes;
+			this.pos = pos;
+		}
 		
 		public MerkleState(Hash hash)
 		{
@@ -126,12 +130,6 @@ public abstract class MerkleHash<State extends MerkleState<State, Hash>, Hash ex
 			return 4 + temp.length + customSize();
 		}
 
-		public Factory<State> factory()
-		{
-			// TODO Auto-generated method stub
-			return null;
-		}
-
 		public boolean sameAs(State obj)
 		{
 			return (this.pos == obj.pos && ArrayUtil.equals(obj.temp, this.temp)) && this.compareCustom(obj);
@@ -142,6 +140,40 @@ public abstract class MerkleHash<State extends MerkleState<State, Hash>, Hash ex
 			Arrays.fill(temp, (byte) 0);
 			temp = null;
 			pos = 0;
+		}
+		
+	}
+	
+	protected static abstract class MerkleStateFactory<State extends MerkleState<State, Hash>, Hash extends MerkleHash<State, Hash>>
+							  extends Factory<MerkleState<State, Hash>>
+	{
+		protected final int size;
+		
+		protected MerkleStateFactory(Class<MerkleState<State, Hash>> class_, int size)
+		{
+			super(class_);
+			this.size = size;
+		}
+		
+		protected abstract State construct(byte[] bytes, int pos);
+		
+		public State resurrect(IIncomingStream is) throws IOException
+		{
+			int pos = is.readInt();
+			byte[] bytes = is.readBytes(size);
+			State state = construct(bytes, pos);
+			state.addCustom(is);
+			return state;
+		}
+		
+		public State resurrect(byte[] bytes, int start)
+		{
+			int pos = Bits.intFromBytes(bytes, start); start += 4;
+			byte[] temp = new byte[size];
+			System.arraycopy(bytes, start, temp, 0, size);
+			State state = construct(temp, pos);
+			state.addCustom(bytes, start);
+			return state;
 		}
 		
 	}
