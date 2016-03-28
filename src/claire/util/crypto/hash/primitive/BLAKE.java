f13ebf72e14cf859341a5fb916d47c346e70aed0
Claire <foxcoindev@gmail.com>
2016-03-28 22:35:34 +1300

    Replaced BLAKE standard class with constructor methods

--- src/claire/util/crypto/hash/primitive/BLAKE.java
+++ src/claire/util/crypto/hash/primitive/BLAKE.java
@@ -2,44 +2,23 @@ package claire.util.crypto.hash.primitive;
 
 import claire.util.standards.crypto.IHash;
 
-public class BLAKE implements IHash {
+public class BLAKE {
 
-	private final IHash blake;
-	
-	public BLAKE(int size) 
+	public IHash<?> getBlake(int size) 
 	{
 		switch(size)
 		{
 			case 224:
-				blake = new BLAKE224();
-				break;
+				return new BLAKE224();
 			case 256:
-				blake = new BLAKE256();
-				break;
+				return new BLAKE256();
 			case 384:
-				blake = new BLAKE384();
-				break;
+				return new BLAKE384();
 			case 512:
-				blake = new BLAKE512();
-				break;
+				return new BLAKE512();
 			default:
 				throw new java.lang.IllegalArgumentException("BLAKE only supports digest sizes of 224, 256, 384, or 512 bits.");
 		}
-	}
-
-	public void add(byte[] bytes, int start, int length)
-	{
-		blake.add(bytes, start, length);
-	}
-
-	public void finish(byte[] out, int start)
-	{
-		blake.finish(out, start);
-	}
-
-	public int outputLength()
-	{
-		return blake.outputLength();
 	}
 
 }
