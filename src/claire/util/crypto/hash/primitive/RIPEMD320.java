632677d57f904f308cb78eea6f8c738ff5c5928f
Claire <foxcoindev@gmail.com>
2016-04-01 21:10:29 +1300

    Made RIPEMD320 conform to new IHash, added tests and fixed bugs

--- src/claire/util/crypto/hash/primitive/RIPEMD320.java
+++ src/claire/util/crypto/hash/primitive/RIPEMD320.java
@@ -11,6 +11,7 @@ import claire.util.memory.Bits;
 import claire.util.memory.util.ArrayUtil;
 import claire.util.standards.IPersistable;
 import claire.util.standards._NAMESPACE;
+import claire.util.standards.crypto.IHash;
 import claire.util.standards.crypto.IState;
 import claire.util.standards.io.IIncomingStream;
 import claire.util.standards.io.IOutgoingStream;
@@ -65,8 +66,9 @@ public final class RIPEMD320
 		reset();
 	}
 	
-	private void reset()
+	public void reset()
 	{
+		super.reset();
 		length = 0;
         STATE[0] = 0x67452301;
         STATE[1] = 0xefcdab89;
@@ -450,11 +452,12 @@ public final class RIPEMD320
 	public static final int test()
 	{
 		RIPEMD320 blake = new RIPEMD320();
+		int i = 0;
+		i += IHash.test(blake);
 		byte[] bytes = new byte[1000];
 		RandUtils.fillArr(bytes);
 		blake.add(bytes);
 		IState state = blake.getState();
-		int i = 0;
 		i += IPersistable.test(state);
 		return i;
 	}
