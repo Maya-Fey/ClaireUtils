92beff3f9e6b55de7e382e5e7e176b4cd9aa565d
Claire <foxcoindev@gmail.com>
2016-04-04 19:14:02 +1200

    Added generic HashFactory to SHA2

--- src/claire/util/crypto/hash/primitive/SHA2.java
+++ src/claire/util/crypto/hash/primitive/SHA2.java
@@ -1,5 +1,6 @@
 package claire.util.crypto.hash.primitive;
 
+import claire.util.crypto.hash.HashFactory;
 import claire.util.standards.crypto.IHash;
 
 public final class SHA2 {
@@ -19,6 +20,35 @@ public final class SHA2 {
 			default:
 				throw new java.lang.IllegalArgumentException("SHA-2 only supports hash lengths of 224, 256, 384, or 512 bits");
 		}
+	}
+	
+	public static HashFactory<MerkleHash<?, ?>> factory()
+	{
+		return factory;
+	}
+	
+	public static final SHA2_Factory factory = new SHA2_Factory();
+	
+	public static final class SHA2_Factory extends HashFactory<MerkleHash<?, ?>>
+	{
+
+		public MerkleHash<?, ?> build(char[] params, char sep)
+		{
+			switch(new String(params))
+			{
+				case "224":
+					return new SHA2_224();
+				case "256":
+					return new SHA2_256();
+				case "384":
+					return new SHA2_384();
+				case "512":
+					return new SHA2_512();
+				default:
+					throw new java.lang.NullPointerException();
+			}
+		}
+		
 	}
 
 }
