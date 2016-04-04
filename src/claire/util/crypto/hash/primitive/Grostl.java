92d530be986dd400ad0bb77c053b89d5156db58c
Claire <foxcoindev@gmail.com>
2016-04-05 08:09:05 +1200

    Added generic Grostl HashFactory

--- src/claire/util/crypto/hash/primitive/Grostl.java
+++ src/claire/util/crypto/hash/primitive/Grostl.java
@@ -1,5 +1,6 @@
 package claire.util.crypto.hash.primitive;
 
+import claire.util.crypto.hash.HashFactory;
 import claire.util.standards.crypto.IHash;
 
 public class Grostl {
@@ -19,6 +20,35 @@ public class Grostl {
 			default:
 				throw new java.lang.IllegalArgumentException("Grostl only supports digest sizes of 224, 256, 384, or 512 bits.");
 		}
+	}
+	
+	public static HashFactory<GrostlCore<?, ?>> factory()
+	{
+		return factory;
+	}
+	
+	public static final GrostlFactory factory = new GrostlFactory();
+	
+	public static final class GrostlFactory extends HashFactory<GrostlCore<?, ?>>
+	{
+
+		public GrostlCore<?, ?> build(char[] params, char sep)
+		{
+			switch(new String(params))
+			{
+				case "224":
+					return new Grostl224();
+				case "256":
+					return new Grostl256();
+				case "384":
+					return new Grostl384();
+				case "512":
+					return new Grostl512();
+				default:
+					throw new java.lang.NullPointerException();
+			}
+		}
+		
 	}
 
 }
