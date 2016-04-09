3719674ec0341ce239549b8f2ea5e3dda5e813f7
Claire <foxcoindev@gmail.com>
2016-04-09 12:18:31 +1200

    Made HashFactory use cryptostring

--- src/claire/util/crypto/hash/HashFactory.java
+++ src/claire/util/crypto/hash/HashFactory.java
@@ -1,9 +1,10 @@
 package claire.util.crypto.hash;
 
+import claire.util.crypto.CryptoString;
 import claire.util.standards.crypto.IHash;
 
 public abstract class HashFactory<Hash extends IHash<?, ?>> {
 	
-	public abstract Hash build(char[] params, char sep);
+	public abstract Hash build(CryptoString str);
 
 }
