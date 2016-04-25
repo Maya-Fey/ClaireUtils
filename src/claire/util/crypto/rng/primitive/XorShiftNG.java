0048d5ff7ad38e8bb374a4eaa52af06936ffad08
Claire <foxcoindev@gmail.com>
2016-04-26 03:11:13 +1200

    UpdateD XorShiftNG to new XorShiftRNG

--- src/claire/util/crypto/rng/primitive/XorShiftNG.java
+++ src/claire/util/crypto/rng/primitive/XorShiftNG.java
@@ -1,15 +1,28 @@
 package claire.util.crypto.rng.primitive;
 
-public class XorShiftNG extends XorShiftRNG {
+public class XorShiftNG 
+	   extends XorShiftRNG {
 	
-	public XorShiftNG(int seed) {
+	public XorShiftNG() 
+	{
+    	super();
+	}
+	
+	public XorShiftNG(boolean b)
+	{
+		super(b);
+	}
+	
+	public XorShiftNG(LongSeed seed)
+	{
 		super(seed);
 	}
-	
-	public XorShiftNG() {
-		
-	}
 
+	public XorShiftNG(long l)
+	{
+		super(l);
+	}
+	
 	protected void update()
 	{
 		this.last ^= (this.last << 21);
