18b24637b7c756a86b69729497781f1c03aea66e
Claire <foxcoindev@gmail.com>
2016-04-26 18:28:58 +1200

    Made JRandom conform to new seed

--- src/claire/util/crypto/rng/primitive/JRandom.java
+++ src/claire/util/crypto/rng/primitive/JRandom.java
@@ -4,26 +4,133 @@ import java.util.Random;
 
 import claire.util.standards.crypto.IRandom;
 
-public class JRandom extends Random implements IRandom {
+public class JRandom 
+	   extends Random 
+	   implements IRandom<LongSeed> {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private LongSeed seed;
 
-	public JRandom(int seed) {
-		super(seed);
-	}
-
-	public JRandom() {
-		super();
-	}
-
-	public byte nextByte()
+	public JRandom() 
 	{
-		return (byte) this.nextInt();
+    	super();
+	}
+	
+	public JRandom(boolean b)
+	{
+		if(b)
+			this.setSeed(new LongSeed((System.currentTimeMillis() ^ (System.nanoTime() << 12))));
+	}
+	
+	public JRandom(LongSeed seed)
+	{
+		super(seed.getSeed());
+		this.seed = seed;
 	}
 
-	public short nextShort()
+	public JRandom(long l)
 	{
-		return (short) this.nextInt();
+		super(l);
+	}
+	
+	public boolean readBool()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public byte readByte()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public short readShort()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public char readChar()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int readInt()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long readLong()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void readBools(boolean[] out, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readNibbles(byte[] out, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readBytes(byte[] out, int off, int bytes)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readShorts(short[] shorts, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readChars(char[] chars, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readInts(int[] ints, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readLongs(long[] longs, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public LongSeed getSeed()
+	{
+		return seed;
+	}
+
+	public void setSeed(LongSeed key)
+	{
+		this.seed = key;
+		super.setSeed(key.getSeed());
+	}
+
+	public void reset()
+	{
+		super.setSeed(seed.getSeed());
+	}
+
+	public void wipe()
+	{
+		super.setSeed(0);
 	}
 
 }
