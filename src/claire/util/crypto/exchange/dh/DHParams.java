078aa0eab38d1092bca0784185c646854e5a6ff0
Claire <foxcoindev@gmail.com>
2016-05-01 01:27:07 +1200

    Made DHParams persistable

--- src/claire/util/crypto/exchange/dh/DHParams.java
+++ src/claire/util/crypto/exchange/dh/DHParams.java
@@ -1,8 +1,16 @@
 package claire.util.crypto.exchange.dh;
 
-import claire.util.math.UInt;
+import java.io.IOException;
 
-public class DHParams {
+import claire.util.io.Factory;
+import claire.util.io.IOUtils;
+import claire.util.math.UInt;
+import claire.util.standards.IPersistable;
+import claire.util.standards.io.IIncomingStream;
+import claire.util.standards.io.IOutgoingStream;
+
+public class DHParams 
+	   implements IPersistable<DHParams> {
 
 	private final UInt modulus;
 	private final UInt generator;
@@ -23,4 +31,47 @@ public class DHParams {
 		return generator;
 	}
 
+	public void export(IOutgoingStream stream) throws IOException
+	{
+		stream.writeIntArr(generator.getArr());
+		stream.writeIntArr(modulus.getArr());
+	}
+
+	public void export(byte[] bytes, int offset)
+	{
+		offset = IOUtils.writeArr(generator.getArr(), bytes, offset);
+		IOUtils.writeArr(modulus.getArr(), bytes, offset);
+	}
+	
+	public int exportSize()
+	{
+		return (modulus.getIntLen() * 4) + (generator.getIntLen() * 4) + 8;
+	}
+
+	public Factory<DHParams> factory()
+	{
+		return factory;
+	}
+	
+	public static final DHParamsFactory factory = new DHParamsFactory();
+	
+	private static final class DHParamsFactory extends Factory<DHParams>
+	{
+		public DHParamsFactory()
+		{
+			super(DHParams.class);
+		}
+
+		public DHParams resurrect(byte[] data, int start) throws InstantiationException
+		{
+			UInt gen = new UInt(IOUtils.readIntArr(data, start)); start += gen.getIntLen() * 4 + 4;
+			UInt mod = new UInt(IOUtils.readIntArr(data, start));
+			return new DHParams(gen, mod);
+		}
+
+		public DHParams resurrect(IIncomingStream stream) throws InstantiationException, IOException
+		{
+			return new DHParams(new UInt(stream.readIntArr()), new UInt(stream.readIntArr()));
+		}
+	}
 }
