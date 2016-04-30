7ff07c15550182bc7d77f5160f358edefec787a1
Claire <foxcoindev@gmail.com>
2016-04-30 19:30:23 +1200

    Added getSeed methods MersenneTwister

--- src/claire/util/crypto/rng/primitive/MersenneTwister.java
+++ src/claire/util/crypto/rng/primitive/MersenneTwister.java
@@ -139,5 +139,50 @@ public class MersenneTwister
 	    mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */ 
 	}
 	
+	public final int[] getSeed(long seed)
+	{	
+	    int[] mt = new int[N];	
+	    mt[0]= (int)(seed & 0xffffffff);
+	    for (mti=1; mti<N; mti++) 
+	    {
+	        mt[mti] = (1812433253 * (mt[mti-1] ^ (mt[mti-1] >>> 30)) + mti); 
+	        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+	        /* In the previous versions, MSBs of the seed affect   */
+	        /* only MSBs of the array mt[].                        */
+	        /* 2002/01/09 modified by Makoto Matsumoto             */
+	    }
+	    return mt;
+	}
+	
+	public int[] getSeed(int[] array)
+	{
+	    if (array.length == 0)
+	        throw new IllegalArgumentException("Array length must be greater than zero");
+	    int i, j, k;
+	    int[] mt = getSeed(19650218);
+	    i=1; j=0;
+	    k = (N>array.length ? N : array.length);
+	    for (; k!=0; k--) 
+	    {
+	        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >>> 30)) * 1664525)) + array[j] + j; /* non linear */
+	        i++;
+	        j++;
+	        if (i>=N) { mt[0] = mt[N-1]; i=1; }
+	        if (j>=array.length) j=0;
+	    }
+	    for (k=N-1; k!=0; k--) 
+	    {
+	        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >>> 30)) * 1566083941)) - i; /* non linear */
+	        i++;
+	        if (i>=N) 
+	        {
+	            mt[0] = mt[N-1]; i=1; 
+	        }
+	    }
+	    mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */ 
+	    return mt;
+	}
+	
+	
 }
 	
