f9f81faf3c065c4c1cc2d040662bd0271e82ef2c
Claire <foxcoindev@gmail.com>
2016-05-01 00:21:45 +1200

    Added key support in StreamCipherRNG

--- src/claire/util/crypto/rng/primitive/StreamCipherRNG.java
+++ src/claire/util/crypto/rng/primitive/StreamCipherRNG.java
@@ -1,16 +1,20 @@
 package claire.util.crypto.rng.primitive;
 
+import java.util.Arrays;
+
 import claire.util.memory.buffer.PrimitiveAggregator;
+import claire.util.standards.crypto.IKey;
 import claire.util.standards.crypto.IRandom;
 import claire.util.standards.crypto.IStreamCipher;
 
-public class StreamCipherRNG implements IRandom {
+public class StreamCipherRNG<Key extends IKey<Key>> 
+	   implements IRandom<Key> {
 
-	private final IStreamCipher<?, ?> cipher;
+	private final IStreamCipher<Key, ?> cipher;
 	private final PrimitiveAggregator agg = new PrimitiveAggregator();
 	private final byte[] buffer = agg.getBuffer();
 	
-	public StreamCipherRNG(IStreamCipher<?, ?> cip)
+	public StreamCipherRNG(IStreamCipher<Key, ?> cip)
 	{
 		this.cipher = cip;
 	}
@@ -46,6 +50,105 @@ public class StreamCipherRNG implements IRandom {
 	private void reqBytes(int bytes)
 	{
 		cipher.fill(buffer, 0, bytes);
+	}
+
+	public boolean readBool()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public byte readByte()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public short readShort()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public char readChar()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int readInt()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long readLong()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void readBools(boolean[] out, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readNibbles(byte[] out, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readBytes(byte[] out, int off, int bytes)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readShorts(short[] shorts, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readChars(char[] chars, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readInts(int[] ints, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void readLongs(long[] longs, int off, int amt)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Key getSeed()
+	{
+		return cipher.getKey();
+	}
+
+	public void setSeed(Key key)
+	{
+		cipher.setKey(key);
+	}
+
+	public void reset()
+	{
+		cipher.reset();
+	}
+
+	public void wipe()
+	{
+		Arrays.fill(buffer, (byte) 0);
+		cipher.wipe();
 	}
 
 }
