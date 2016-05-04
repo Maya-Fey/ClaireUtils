e50e0ec6967a46391fd36a3fe05e74da72691f6f
Claire <foxcoindev@gmail.com>
2016-05-04 19:16:55 +1200

    Added KeyFactory

---
+++ src/claire/util/crypto/KeyFactory.java
@@ -0,0 +1,16 @@
+package claire.util.crypto;
+
+import claire.util.io.Factory;
+import claire.util.standards.crypto.IRandom;
+
+public abstract class KeyFactory<Type> 
+				extends Factory<Type> {
+
+	public KeyFactory(Class<Type> class_) 
+	{
+		super(class_);
+	}
+
+	public abstract Type random(IRandom<?, ?> rand, CryptoString s);
+
+}
