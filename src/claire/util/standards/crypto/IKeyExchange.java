b5c89998a873a29bcbe0450382960616e30be79d
Claire <foxcoindev@gmail.com>
2016-05-13 23:45:13 +1200

    Redid IKeyExchange

--- src/claire/util/standards/crypto/IKeyExchange.java
+++ src/claire/util/standards/crypto/IKeyExchange.java
@@ -1,12 +1,25 @@
 package claire.util.standards.crypto;
 
-import claire.util.standards.CObject;
+import claire.util.crypto.KeyFactory;
 
-public interface IKeyExchange<Type extends IKeyExchange<Type>>
-	   extends CObject<Type> {
+public interface IKeyExchange<Private extends IKey<Private>, Public extends IKey<Public>> {
 
-	void absorb(Type t);
-	Type create(Type t);
+	void setPrivate(Private key);
+	Private getPrivate();
+	
+	Public genPublic();
+	
+	void output(Public other, byte[] bytes, int start);
+	int outputLen();
+	
+	default byte[] output(Public other)
+	{
+		byte[] bytes = new byte[outputLen()];
+		output(other, bytes, 0);
+		return bytes;
+	}
+	
+	KeyFactory<Private> keyFactory();
 	
 	void erase();
 	
