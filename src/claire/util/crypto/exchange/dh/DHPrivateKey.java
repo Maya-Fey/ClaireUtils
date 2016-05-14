4e555cbcbda73906a1ef5acfa9b30c70c727d36e
Claire <foxcoindev@gmail.com>
2016-05-15 11:41:44 +1200

    Gave factory to DHPrivateKey

--- src/claire/util/crypto/exchange/dh/DHPrivateKey.java
+++ src/claire/util/crypto/exchange/dh/DHPrivateKey.java
@@ -2,10 +2,14 @@ package claire.util.crypto.exchange.dh;
 
 import java.io.IOException;
 
+import claire.util.crypto.CryptoString;
 import claire.util.crypto.KeyFactory;
+import claire.util.math.MathHelper;
 import claire.util.math.UInt;
 import claire.util.standards._NAMESPACE;
 import claire.util.standards.crypto.IKey;
+import claire.util.standards.crypto.IRandom;
+import claire.util.standards.io.IIncomingStream;
 import claire.util.standards.io.IOutgoingStream;
 
 public class DHPrivateKey 
@@ -67,11 +71,35 @@ public class DHPrivateKey
 		exp = null;
 	}
 
-	@Override
 	public KeyFactory<DHPrivateKey> factory()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new DHPrivateKeyFactory(dh);
+	}
+	
+	public static final class DHPrivateKeyFactory extends KeyFactory<DHPrivateKey>
+	{
+		private final DHParams params;
+		
+		public DHPrivateKeyFactory(DHParams params)
+		{
+			super(DHPrivateKey.class);
+			this.params = params;
+		}
+
+		public DHPrivateKey random(IRandom<?, ?> rand, CryptoString s)
+		{
+			return new DHPrivateKey(params, MathHelper.randomIntegerGood(params.getModulus(), rand));
+		}
+
+		public DHPrivateKey resurrect(byte[] data, int start) throws InstantiationException
+		{
+			return new DHPrivateKey(params, UInt.factory.resurrect(data, start));
+		}
+
+		public DHPrivateKey resurrect(IIncomingStream stream) throws InstantiationException, IOException
+		{
+			return new DHPrivateKey(params, stream.resurrect(UInt.factory));
+		}
 	}
 
 }
