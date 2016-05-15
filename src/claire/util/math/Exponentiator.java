a2bce7aa08726c56bb0dc70ca19269dd43786d31
Claire <foxcoindev@gmail.com>
2016-05-16 08:15:23 +1200

    Added return methods to exponentiator

--- src/claire/util/math/Exponentiator.java
+++ src/claire/util/math/Exponentiator.java
@@ -198,4 +198,92 @@ public class Exponentiator<Int extends IInteger<Int>> {
 		i.p_modulo(mod);
 	}
 	
+	/**
+	 * This method takes the exponent of an IInteger by a 32-bit integer
+	 * <br><br>
+	 * Expects:
+	 * <ul>
+	 * <li>An IInteger of any value</li>
+	 * <li>An Integer greater then or equal to zero</li>
+	 * </ul>
+	 * An negative exponent will result in undefined behavior.
+	 * <br><br>
+	 * Returns: Integer of specified type
+	 */
+	public Int exponent(final Int n, int exponent)
+	{
+		Int i = n.createDeepClone();
+		if(exponent == 0) {
+			i.setTo(1);
+			return i;
+		} 
+		if(exponent == 1)
+			return i;
+		o.setTo(1);
+		while(exponent > 1)
+		{
+			if((exponent & 1) == 1) 
+				o.p_multiply(i);
+			i.p_square();
+			exponent >>>= 1;
+		}
+		i.p_multiply(o);
+		return i;
+	}
+	
+	/**
+	 * This method takes the exponent of an IInteger by another IInteger
+	 * <br><br>
+	 * Expects:
+	 * <ul>
+	 * <li>An IInteger of any value</li>
+	 * <li>An IInteger greater then or equal to zero</li>
+	 * </ul>
+	 * An negative exponent will result in undefined behavior.
+	 * <br><br>
+	 * Returns: Integer of specified type
+	 */
+	public Int exponent(final Int i, final IInteger<?> exponent)
+	{
+		if(MathHelper.getRealLength(exponent.getArr()) == 1)
+			return exponent(i, exponent.getArr()[0]);
+		else
+			return exponent_sure(i, exponent);
+	}
+	
+	/**
+	 * This method takes the exponent of an IInteger by another IInteger, without
+	 * testing if the number can fit in 32 bits.
+	 * <br><br>
+	 * Expects:
+	 * <ul>
+	 * <li>An IInteger of any value</li>
+	 * <li>An IInteger greater then or equal to zero</li>
+	 * </ul>
+	 * An negative exponent will result in undefined behavior.
+	 * <br><br>
+	 * Returns: Integer of specified type
+	 */
+	public Int exponent_sure(final Int n, final IInteger<?> exponent)
+	{
+		Int i = n.createDeepClone();
+		if(!exponent.isNonZero()) {
+			i.setTo(1);
+			return i;
+		}
+		if(exponent.isEqualTo(1))
+			return i;
+		final int max = MathHelper.getMSB(exponent.getArr());
+		int bit = 0;
+		o.setTo(1);
+		while(bit < max)
+		{
+			if(exponent.bitAt(bit++)) 
+				o.p_multiply(i);
+			i.p_square();			
+		}
+		i.p_multiply(o);
+		return i;
+	}
+	
 }
