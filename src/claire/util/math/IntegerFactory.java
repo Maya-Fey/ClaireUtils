b0b32f0ee32ae4466edc4aef356307cf7cfd98bb
Claire <foxcoindev@gmail.com>
2016-05-17 17:17:58 +1200

    Added string construction methods to IInteger

--- src/claire/util/math/IntegerFactory.java
+++ src/claire/util/math/IntegerFactory.java
@@ -1,5 +1,7 @@
 package claire.util.math;
 
+import claire.util.encoding.CString;
+import claire.util.encoding.PartialString;
 import claire.util.standards.IInteger;
 
 public abstract class IntegerFactory<Int extends IInteger<?>> {
@@ -11,6 +13,21 @@ public abstract class IntegerFactory<Int extends IInteger<?>> {
 	public Int construct(char[] chars, int len)
 	{
 		return construct(chars, 0, chars.length, len);
+	}
+	
+	public Int construct(String s, int len)
+	{
+		return construct(s.toCharArray(), 0, s.length(), len);
+	}
+	
+	public Int construct(CString s, int len)
+	{
+		return construct(s.array(), 0, s.length(), len);
+	}
+	
+	public Int construct(PartialString s, int len)
+	{
+		return construct(s.array(), s.getOffset(), s.getLength(), len);
 	}
 
 }
