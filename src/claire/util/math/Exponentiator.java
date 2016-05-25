9b624b0127fb4654c1130ff336969ef582e9a886
Claire <foxcoindev@gmail.com>
2016-05-26 08:35:13 +1200

    Updated exponent() to new method

--- src/claire/util/math/Exponentiator.java
+++ src/claire/util/math/Exponentiator.java
@@ -220,15 +220,15 @@ public class Exponentiator<Int extends IInteger<Int>> {
 		} 
 		if(exponent == 1)
 			return i;
-		o.setTo(1);
-		while(exponent > 1)
+		o.setTo(i);
+		int max = Bits.getMSB(exponent);
+		int bit = max - 1;
+		while(bit > -1)
 		{
-			if((exponent & 1) == 1) 
-				o.p_multiply(i);
 			i.p_square();
-			exponent >>>= 1;
+			if(Bits.getBit(exponent, bit--)) 
+				i.p_multiply(o);
 		}
-		i.p_multiply(o);
 		return i;
 	}
 	
