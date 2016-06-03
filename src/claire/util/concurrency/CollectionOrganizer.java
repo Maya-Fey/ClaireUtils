f9297a6c1454f7d5a5f5300f5cdc04d121596b26
Claire <foxcoindev@gmail.com>
2016-06-04 11:25:42 +1200

    Added CollectionOrganizer

---
+++ src/claire/util/concurrency/CollectionOrganizer.java
@@ -0,0 +1,43 @@
+package claire.util.concurrency;
+
+
+public class CollectionOrganizer<Type> 
+	   extends TaskMonitor {
+	
+	private final Basket<Type> basket;
+	private final GeneratorThread<Type>[] gens;
+	
+	public CollectionOrganizer(Basket<Type> basket, GeneratorThread<Type>[] gens)
+	{
+		this.basket = basket;
+		this.gens = gens;
+	}
+	
+	public boolean isDone()
+	{
+		return !basket.hasSpace();
+	}
+
+	public void onProgress() {}
+	
+	public void notifyProgress()
+	{
+		if(this.isDone())
+			synchronized(this) {
+				this.notify();
+			}
+	}
+	
+	public void waitOn()
+	{		
+		synchronized(this) {
+			while(!isDone())
+				try {
+					this.wait();
+				} catch (InterruptedException e) {}
+		}
+		for(GeneratorThread<Type> g : gens)
+			g.interrupt();
+	}
+
+}
