58004bb18c39e523312e1fedc134d9206290a2bd
Claire <foxcoindev@gmail.com>
2016-08-05 09:18:55 +1200

    Finalized log methods for JVM inlining

--- src/claire/util/logging/BasicLogger.java
+++ src/claire/util/logging/BasicLogger.java
@@ -13,133 +13,133 @@ public abstract class BasicLogger
 	protected abstract void add(char ch);
 	protected abstract void add(char[] chars);
 
-	public void print(String s)
+	public final void print(String s)
 	{
 		add(s.toCharArray());
 	}
 
-	public void print(CString s)
+	public final void print(CString s)
 	{
 		add(s.array());
 	}
 
-	public void print(char ... chars)
+	public final void print(char ... chars)
 	{
 		add(chars);
 	}
 
-	public void print(byte ... bytes)
+	public final void print(byte ... bytes)
 	{
 		add(Hex.toHex(bytes));
 	}
 
-	public void print(boolean bool)
+	public final void print(boolean bool)
 	{
 		add(EncodingUtil.booleanText(bool));
 	}
 
-	public void print(byte b)
+	public final void print(byte b)
 	{
 		add(Base10.fromByte(b));
 	}
 
-	public void print(short b)
+	public final void print(short b)
 	{
 		add(Base10.fromShort(b));
 	}
 
-	public void print(char b)
+	public final void print(char b)
 	{
 		add(b);
 	}
 
-	public void print(int b)
+	public final void print(int b)
 	{
 		add(Base10.fromInt(b));
 	}
 
-	public void print(long b)
+	public final void print(long b)
 	{
 		add(Base10.fromLong(b));
 	}
 
-	public void print(Object o)
+	public final void print(Object o)
 	{
 		add(o.toString().toCharArray());
 	}
 	
-	public void print(IDebuggable o)
+	public final void print(IDebuggable o)
 	{
 		add(o.toCString().array());
 	}
 
-	public void println(String s)
+	public final void println(String s)
 	{
 		this.print(s);
 		this.println();
 	}
 
-	public void println(CString s)
+	public final void println(CString s)
 	{
 		this.print(s);
 		this.println();
 	}
 
-	public void println(char ... chars)
+	public final void println(char ... chars)
 	{
 		this.print(chars);
 		this.println();
 	}
 
-	public void println(byte ... bytes)
+	public final void println(byte ... bytes)
 	{
 		this.print(bytes);
 		this.println();
 	}
 
-	public void println(boolean bool)
+	public final void println(boolean bool)
 	{
 		this.print(bool);
 		this.println();
 	}
 
-	public void println(byte b)
+	public final void println(byte b)
 	{
 		this.print(b);
 		this.println();
 	}
 
-	public void println(short b)
+	public final void println(short b)
 	{
 		this.print(b);
 		this.println();
 	}
 
-	public void println(char b)
+	public final void println(char b)
 	{
 		this.print(b);
 		this.println();
 	}
 
-	public void println(int b)
+	public final void println(int b)
 	{
 		this.print(b);
 		this.println();
 	}
 
-	public void println(long b)
+	public final void println(long b)
 	{
 		this.print(b);
 		this.println();
 	}
 
-	public void println(Object o)
+	public final void println(Object o)
 	{
 		this.print(o);
 		this.println();
 	}
 	
-	public void println(IDebuggable o)
+	public final void println(IDebuggable o)
 	{
 		this.print(o);
 		this.println();
