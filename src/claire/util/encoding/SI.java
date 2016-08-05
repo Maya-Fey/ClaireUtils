4dba971d7a9cb8e96a06afb60c1dbbfc0d4aeb0b
Claire <foxcoindev@gmail.com>
2016-08-05 21:53:45 +1200

    Added toSI

--- src/claire/util/encoding/SI.java
+++ src/claire/util/encoding/SI.java
@@ -2,5 +2,18 @@ package claire.util.encoding;
 
 public class SI
 {
+	private static final String[] PREFIX = new String[] {
+		"", "K", "M", "G", "T", "P", "E", "Z", "Y"
+	};
+	
+	public static String toSI(String unit, long amt)
+	{
+		int pos = 0;
+		while(amt > 1000) {
+			pos++;
+			amt /= 1000;
+		}
+		return PREFIX[pos] + unit + amt;
+	}
 	
 }
