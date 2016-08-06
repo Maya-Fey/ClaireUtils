b2c4837bf459cbb96854fc895b60192200990223
Claire <foxcoindev@gmail.com>
2016-08-06 12:33:10 +1200

    Added barebones IDEA

---
+++ src/claire/util/crypto/cipher/primitive/block/IDEA.java
@@ -0,0 +1,70 @@
+package claire.util.crypto.cipher.primitive.block;
+
+import claire.util.crypto.cipher.key.block.KeyIDEA;
+import claire.util.standards.crypto.ISymmetric;
+
+public class IDEA 
+	   implements ISymmetric<KeyIDEA>
+{
+
+	public KeyIDEA getKey()
+	{
+		// TODO Fix yo shiet
+		return null;
+	}
+
+	public void setKey(KeyIDEA t)
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public void encryptBlock(byte[] block, int start)
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public void encryptBlock(byte[] block, int start0, byte[] out, int start1)
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public void reset()
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public void wipe()
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public int plaintextSize()
+	{
+		// TODO Fix yo shiet
+		return 0;
+	}
+
+	public int ciphertextSize()
+	{
+		// TODO Fix yo shiet
+		return 0;
+	}
+
+	public void decryptBlock(byte[] block, int start)
+	{
+		// TODO Fix yo shiet
+		
+	}
+
+	public void decryptBlock(byte[] block, int start0, byte[] out, int start1)
+	{
+		// TODO Fix yo shiet
+		
+	}
+	
+}
