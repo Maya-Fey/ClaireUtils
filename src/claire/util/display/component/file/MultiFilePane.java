f216dde8da5eb5822993335dba77cd81ebf0b525
Claire <foxcoindev@gmail.com>
2016-08-30 18:02:43 +1200

    Made MultiFilePane extends SMultiFilePane

--- src/claire/util/display/component/file/MultiFilePane.java
+++ src/claire/util/display/component/file/MultiFilePane.java
@@ -1,6 +1,28 @@
 package claire.util.display.component.file;
 
+import java.io.File;
+
 public class MultiFilePane
+	   extends SMultiFilePane
 {
+
+	public MultiFilePane(int gbc)
+	{
+		super(gbc);
+	}
+
+	@Override
+	public boolean hasSelected()
+	{
+		// TODO Fix yo shiet
+		return false;
+	}
+
+	@Override
+	public File[] getFiles()
+	{
+		// TODO Fix yo shiet
+		return null;
+	}
 	
 }
