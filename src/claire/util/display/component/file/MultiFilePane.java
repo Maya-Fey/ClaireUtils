d1637b51f57decce5978c6be8537746dc67118bd
Claire <foxcoindev@gmail.com>
2016-09-02 09:49:07 +1200

    Added selection support valueChanged MultiFilePane

--- src/claire/util/display/component/file/MultiFilePane.java
+++ src/claire/util/display/component/file/MultiFilePane.java
@@ -198,8 +198,10 @@ public class MultiFilePane
 				for(int i : selected)
 					total += current[i].length();
 				status.setText(selected.length + " files selected, totalling " + total + " bytes.");
+				this.selected = null;
 			} else 
 				status.setText("Both files and folders selected");
+			this.selected = null;
 		}
 	}
 
