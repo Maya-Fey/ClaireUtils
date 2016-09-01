afc2c0e40bf0c9ff1265d1e81e6dd8eb47764bf9
Claire <foxcoindev@gmail.com>
2016-09-02 11:08:54 +1200

    Made MultiFileSelectionMessage extend BasicMessage

--- src/claire/util/display/message/MultiFileSelectionMessage.java
+++ src/claire/util/display/message/MultiFileSelectionMessage.java
@@ -1,6 +1,18 @@
 package claire.util.display.message;
 
+import java.awt.Window;
+
+import javax.swing.JPanel;
+
 public class MultiFileSelectionMessage
+	   extends BasicMessage
 {
+
+	private static final long serialVersionUID = 5754140720953562887L;
+
+	public MultiFileSelectionMessage(Window arg0, JPanel panel, String message)
+	{
+		super(arg0, panel, message);
+	}
 	
 }
