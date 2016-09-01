878fd05ca7187a5bfdb54bcbc0e6145aab105d94
Claire <foxcoindev@gmail.com>
2016-09-02 11:15:03 +1200

    Added newFilePane MultiFileSelectionMessage

--- src/claire/util/display/message/MultiFileSelectionMessage.java
+++ src/claire/util/display/message/MultiFileSelectionMessage.java
@@ -1,8 +1,11 @@
 package claire.util.display.message;
 
 import java.awt.Window;
+import java.io.File;
 
 import javax.swing.JPanel;
+
+import claire.util.display.component.file.SMultiFilePane;
 
 public class MultiFileSelectionMessage
 	   extends BasicMessage
@@ -13,6 +16,15 @@ public class MultiFileSelectionMessage
 	public MultiFileSelectionMessage(Window arg0, JPanel panel, String message)
 	{
 		super(arg0, panel, message);
+	}
+	
+	public static final SMultiFilePane newMultiFilePane(Window owner, MultiFileSelectionMessage m, File f, int type)
+	{
+		switch(type)
+		{
+			default:
+				throw new java.lang.NullPointerException("Invalid type");
+		}
 	}
 	
 }
