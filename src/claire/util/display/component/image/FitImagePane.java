72194cd095f1d7b009fa91c9aa9222424fccac2c
Claire <foxcoindev@gmail.com>
2016-09-03 01:50:35 +1200

    Made FitImagePane extend ImagePane

--- src/claire/util/display/component/image/FitImagePane.java
+++ src/claire/util/display/component/image/FitImagePane.java
@@ -1,6 +1,14 @@
 package claire.util.display.component.image;
 
+import claire.util.display.component.ImagePane;
+
 public class FitImagePane
+	   extends ImagePane
 {
+
+	protected void drawImage()
+	{
+		
+	}
 	
 }
