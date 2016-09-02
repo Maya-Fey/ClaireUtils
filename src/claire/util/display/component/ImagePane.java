550a72939e115b694cdbce3ead1172dccf18733f
Claire <foxcoindev@gmail.com>
2016-09-03 01:21:08 +1200

    Added barebones ImagePane

---
+++ src/claire/util/display/component/ImagePane.java
@@ -0,0 +1,10 @@
+package claire.util.display.component;
+
+import javax.swing.JComponent;
+
+public class ImagePane 
+	   extends JComponent
+{
+	private static final long serialVersionUID = 1L;
+	
+}
