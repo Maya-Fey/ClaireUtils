1d84c6772e57eff473146cbb33c6c75e15d690e2
Claire <foxcoindev@gmail.com>
2016-09-03 01:56:14 +1200

    Made StretchImagePane extend ImagePane

--- src/claire/util/display/component/image/StretchImagePane.java
+++ src/claire/util/display/component/image/StretchImagePane.java
@@ -1,6 +1,19 @@
 package claire.util.display.component.image;
 
+import java.awt.Graphics;
+
+import claire.util.display.component.ImagePane;
+
 public class StretchImagePane
+	   extends ImagePane
 {
+
+	private static final long serialVersionUID = -2530653933232324940L;
+
+	protected void drawImage(Graphics g)
+	{
+		// TODO Fix yo shiet
+		
+	}
 	
 }
