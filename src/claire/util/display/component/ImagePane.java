06222d65bc736a74936b46bc48e101b5028dfff3
Claire <foxcoindev@gmail.com>
2016-09-03 01:22:17 +1200

    Added drawImage() ImagePane

--- src/claire/util/display/component/ImagePane.java
+++ src/claire/util/display/component/ImagePane.java
@@ -2,9 +2,10 @@ package claire.util.display.component;
 
 import javax.swing.JComponent;
 
-public class ImagePane 
-	   extends JComponent
+public abstract class ImagePane 
+				extends JComponent
 {
 	private static final long serialVersionUID = 1L;
 	
+	protected abstract void drawImage();
 }
