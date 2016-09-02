018c4ba981eb7425f0dee1f896dfce3c7a67ce1b
Claire <foxcoindev@gmail.com>
2016-09-03 01:35:38 +1200

    Added setImage() ImagePane

--- src/claire/util/display/component/ImagePane.java
+++ src/claire/util/display/component/ImagePane.java
@@ -1,5 +1,7 @@
 package claire.util.display.component;
 
+import java.awt.Image;
+
 import javax.swing.JComponent;
 
 public abstract class ImagePane 
@@ -7,5 +9,13 @@ public abstract class ImagePane
 {
 	private static final long serialVersionUID = 1L;
 	
+	protected Image img;
+	
 	protected abstract void drawImage();
+
+	public void setImage(Image img)
+	{
+		this.img = img;
+	}
+	
 }
