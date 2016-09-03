e1b4894f51ba0c4f7776202e599bb38013bce98a
Claire <foxcoindev@gmail.com>
2016-09-04 10:53:50 +1200

    Added initialize call InformationCollectionMessage

--- src/claire/util/display/message/InformationCollectionMessage.java
+++ src/claire/util/display/message/InformationCollectionMessage.java
@@ -28,6 +28,7 @@ public class InformationCollectionMessage
 	{
 		super(arg0, message);
 		this.panel = panel;
+		panel.initialize();
 		TablePane table = new TablePane(GridBagConstraints.BOTH);
 		table.newRow(1.0D);
 		if(cancel) {
