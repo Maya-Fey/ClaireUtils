a6e5b3df9a7cb669fcca87ab230b14adf843af65
Claire <foxcoindev@gmail.com>
2016-09-09 00:20:15 +1200

    Made component ListSelectionPane

--- src/claire/util/display/component/ListSelectionPane.java
+++ src/claire/util/display/component/ListSelectionPane.java
@@ -1,6 +1,10 @@
 package claire.util.display.component;
 
+import javax.swing.JPanel;
+
 public class ListSelectionPane
+	   extends JPanel
 {
+	private static final long serialVersionUID = 1L;
 	
 }
