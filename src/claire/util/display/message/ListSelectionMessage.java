b9dc2d0a411f00d7084650704ca630d58a222c03
Claire <foxcoindev@gmail.com>
2016-09-09 03:27:06 +1200

    Added BasicMessage extension ListSelectionMessage

--- src/claire/util/display/message/ListSelectionMessage.java
+++ src/claire/util/display/message/ListSelectionMessage.java
@@ -1,7 +1,16 @@
 package claire.util.display.message;
 
+import java.awt.Window;
 
 public class ListSelectionMessage
+	   extends BasicMessage
 {
+
+	private static final long serialVersionUID = -8679876580683360350L;
+
+	public ListSelectionMessage(Window arg0, String message)
+	{
+		super(arg0, message);
+	}
 	
 }
