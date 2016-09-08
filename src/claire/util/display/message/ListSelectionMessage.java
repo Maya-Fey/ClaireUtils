6cf7e1ecd3826957de69cab744ff3007644fabd7
Claire <foxcoindev@gmail.com>
2016-09-09 03:28:30 +1200

    Added ActionListener implementation ListSelectionMessage

--- src/claire/util/display/message/ListSelectionMessage.java
+++ src/claire/util/display/message/ListSelectionMessage.java
@@ -1,9 +1,12 @@
 package claire.util.display.message;
 
 import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ListSelectionMessage
 	   extends BasicMessage
+	   implements ActionListener
 {
 
 	private static final long serialVersionUID = -8679876580683360350L;
@@ -11,6 +14,14 @@ public class ListSelectionMessage
 	public ListSelectionMessage(Window arg0, String message)
 	{
 		super(arg0, message);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		switch(e.getActionCommand())
+		{
+			
+		}
 	}
 	
 }
