28c949ba1cf4f165eae5f28fa657e453157cb83c
Claire <foxcoindev@gmail.com>
2016-09-11 12:36:22 +1200

    Extended JPanel MultiListSelectionPane

--- src/claire/util/display/component/MultiListSelectionPane.java
+++ src/claire/util/display/component/MultiListSelectionPane.java
@@ -1,6 +1,11 @@
 package claire.util.display.component;
 
+import javax.swing.JPanel;
+
 public class MultiListSelectionPane
+	   extends JPanel
 {
+
+	private static final long serialVersionUID = 2315352448715873384L;
 	
 }
