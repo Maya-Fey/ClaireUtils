733cc2822c8a20fc863a9d3a2daef5f96e29f7db
Claire <foxcoindev@gmail.com>
2016-09-11 12:59:45 +1200

    Added pane var MultiListSelectionMessage

--- src/claire/util/display/message/MultiListSelectionMessage.java
+++ src/claire/util/display/message/MultiListSelectionMessage.java
@@ -4,16 +4,21 @@ import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import claire.util.display.component.MultiListSelectionPane;
+
 public class MultiListSelectionMessage
 	   extends BasicMessage
 	   implements ActionListener
 {
 
 	private static final long serialVersionUID = 7603321529435719756L;
-
-	public MultiListSelectionMessage(Window arg0, String message)
+	
+	private final MultiListSelectionPane pane;
+	
+	public MultiListSelectionMessage(Window arg0, String title, String ok, String message, String[] elements)
 	{
-		super(arg0, message);
+		super(arg0, title);
+		this.pane = new MultiListSelectionPane(this, elements, message);
 	}
 
 	public void actionPerformed(ActionEvent arg0)
