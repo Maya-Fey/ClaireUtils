66c086df5ec6243bdfe4828a2c273544d0609313
Claire <foxcoindev@gmail.com>
2016-09-11 12:55:44 +1200

    Extended BasicMessage MultiListSelectionMessage

--- src/claire/util/display/message/MultiListSelectionMessage.java
+++ src/claire/util/display/message/MultiListSelectionMessage.java
@@ -1,6 +1,16 @@
 package claire.util.display.message;
 
+import java.awt.Window;
+
 public class MultiListSelectionMessage
+	   extends BasicMessage
 {
+
+	private static final long serialVersionUID = 7603321529435719756L;
+
+	public MultiListSelectionMessage(Window arg0, String message)
+	{
+		super(arg0, message);
+	}
 	
 }
